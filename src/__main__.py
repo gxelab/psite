e16b1ac79b7d3c91f50452372bcb497fe7142a8b
mt1022 <mt1022@users.noreply.github.com>
2022-04-28 09:44:11 +0800

    test click as cli

--- src/__main__.py
+++
@@ -1,5 +0,0 @@
-from . import hello_world
-
-if __name__ == '__main__':
-    hello_world()
-
---
+++ src/main.py
@@ -0,0 +1,7 @@
+import click
+
+@click.command()
+def psite():
+    """Example script."""
+    click.echo('Hello World!')
+
