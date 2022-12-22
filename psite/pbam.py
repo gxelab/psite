c9a93bc4d5125ec16c5b1e7c27adb0a7a512bf27
Hong Zhang <mt1022@users.noreply.github.com>
2022-12-22 22:00:15 +0800

    compatibility change for new model

--- psite/pbam.py
+++ psite/pbam.py
@@ -5,7 +5,8 @@ import click
 import pandas as pd
 import numpy as np
 import pysam
-from psite.utils import read_fasta, chunk_iter, rev_comp, CLICK_CS
+# from psite.utils import read_fasta, chunk_iter, rev_comp, CLICK_CS
+from utils import read_fasta, chunk_iter, rev_comp, CLICK_CS
 
 
 @click.command(context_settings=CLICK_CS)
@@ -16,21 +17,17 @@ from psite.utils import read_fasta, chunk_iter, rev_comp, CLICK_CS
 @click.option('-f', '--out_format', default='bam',
               type=click.Choice(['bam', 'sam']),
               help='P-site alignemnt output format')
-@click.option('-c', '--chunk_size', type=click.INT, default=65536,
-              help='chunk size for prediction batch')
 @click.option('-i', '--ignore_txversion', is_flag=True, default=False,
               help='whether to ignore trasncript version in ".\d+" format')
-@click.option('-l', '--rlen_min', type=click.INT, default=27,
-              help='lower bound for RPF mapped length')
-@click.option('-u', '--rlen_max', type=click.INT, default=32,
+@click.option('-l', '--rlen_min', type=click.INT, default=None,
+              help='lower bound for mapped mapped length')
+@click.option('-u', '--rlen_max', type=click.INT, default=None,
               help='upper bound for mapped read length')
-@click.option('-n', '--nts', type=click.INT, default=1,
-              help='fanking nucleotides to consider at each side')
-@click.option('-p', '--threads', type=click.INT, default=1,
-              help='Number of threads used for prediction')
+@click.option('-c', '--chunk_size', type=click.INT, default=100000,
+              help='chunk size for prediction batch')
 def pbam(path_ref, path_bam, path_model, path_out, out_format='bam',
-            rlen_min=25, rlen_max=35, ignore_txversion=False, nts=3,
-            chunk_size=1024, threads=1):
+            ignore_txversion=False, chunk_size=100000,
+            rlen_min=None, rlen_max=None):
     """
     generate bam with only P-site regions
     
@@ -44,57 +41,60 @@ def pbam(path_ref, path_bam, path_model, path_out, out_format='bam',
     # gzipped fasta is detected automatically
     ref = read_fasta(path_ref, ignore_version=ignore_txversion)
     model = pickle.load(open(path_model, 'rb'))
-    model.n_jobs = threads
+    # nts should be set only once during training
+    nts = model.nts
+
+    # set read length cutoffs based on those used for training if not set
+    if rlen_min == None:
+        rlen_min = model.qwidth_range.min()
+    if rlen_max == None:
+        rlen_max = model.qwidth_range.max()
     
     # prediction
     print('...Parse bam by chunk and predict', file=sys.stderr)
-    batch_cols = ['qwidth'] + [f'{i}{j}' for i in ['s', 'e'] for j in range(2*nts)]
-    features = ['qwidth'] + [f'{i}{j}_{k}' for i in ['s', 'e'] for j in range(2*nts) for k in 'ACGT']
+    nuc_cols = [f's{i}' for i in range(2*nts)]
 
     with pysam.AlignmentFile(path_bam) as bam:
         if out_format == 'bam':
             output = pysam.AlignmentFile(path_out, "wb", header=bam.header)
         else:
             output =  pysam.AlignmentFile(path_out, "w", header=bam.header)
-        for chunk in chunk_iter(bam, chunk_size):
-            # print('...reading new chunk: ' + time(), file=sys.stderr)
+        for chunk in chunk_iter(bam, size=chunk_size):
             aligns = list(chunk)
             # filter missing tx
             aligns = [i for i in aligns if i.reference_name in ref]
             if len(aligns) == 0:
                 continue
             qlen = np.zeros(len(aligns), dtype=int)
-            seqs_start = [None] * len(aligns)
-            seqs_end = [None] * len(aligns)
+            nucs = [None] * len(aligns)
             for i, align in enumerate(aligns):
+                seq = ref[align.reference_name]
                 qlen[i] = align.query_alignment_length
-                if align.reference_start - nts >= 0:
-                    seq_left = ref[align.reference_name][(align.reference_start - nts):(align.reference_start + nts)]
-                else:
-                    seq_left = ref[align.reference_name][:(align.reference_start + nts)].rjust(2*nts, '-')
-                if align.reference_end + nts <= len(ref[align.reference_name]):
-                    seq_right = ref[align.reference_name][(align.reference_end - nts):(align.reference_end + nts)]
-                else:
-                    seq_right = ref[align.reference_name][(align.reference_end - nts):].ljust(2*nts, '-')
                 if align.is_reverse:
-                    seqs_start[i] = rev_comp(seq_right)
-                    seqs_end[i] = rev_comp(seq_left)
+                    if align.reference_end + nts <= len(seq):
+                        flank_5p = seq[(align.reference_end - nts):(align.reference_end + nts)]
+                    else:
+                        flank_5p = seq[(align.reference_end - nts):].ljust(2*nts, '-')
+                    nucs[i] = rev_comp(flank_5p)
                 else:
-                    seqs_start[i] = seq_left
-                    seqs_end[i] = seq_right
-            seqs_start = pd.DataFrame([list(i) for i in seqs_start])
-            seqs_end = pd.DataFrame([list(i) for i in seqs_end])
+                    if align.reference_start - nts >= 0:
+                        flank_5p = seq[(align.reference_start - nts):(align.reference_start + nts)]
+                    else:
+                        flank_5p = seq[:(align.reference_start + nts)].rjust(2*nts, '-')
+                    nucs[i] = flank_5p
+            nucs = pd.DataFrame([list(i) for i in nucs])
             # index needed for edge case of one-row data frame
             batch = pd.DataFrame({'qwidth': qlen}, index=range(len(qlen)))
-            batch = pd.concat([batch, seqs_start, seqs_end], axis = 1)
-            batch.set_axis(batch_cols, axis=1, inplace=True)
-            batch = pd.get_dummies(batch)
-            X = np.zeros((batch.shape[0], 1 + 16 * nts), dtype=int)
-            for k, column in enumerate(features):
-                if column in list(batch.columns.values):
-                    X[:,k] = batch.loc[:, column]
-            p_sites = model.predict(X)
+            batch = pd.concat([batch, nucs], axis = 1)
+            batch.set_axis(['qwidth'] + nuc_cols, axis=1, inplace=True)
+            for i in nuc_cols:
+                batch.loc[:, i] = pd.Categorical(batch[i], categories=['A', 'C', 'G', 'T'])
+            batch = pd.get_dummies(batch, drop_first=True)
+            
+            p_sites = model.predict(batch)
             for p, align in zip(p_sites, aligns):
+                if align.query_alignment_length < rlen_min or align.query_alignment_length > rlen_max:
+                    continue
                 a = pysam.AlignedSegment()
                 a.query_name = align.query_name
                 a.flag = align.flag
@@ -102,8 +102,6 @@ def pbam(path_ref, path_bam, path_model, path_out, out_format='bam',
                 a.mapping_quality = align.mapping_quality
                 a.tags = align.tags
                 if align.is_reverse:
-                    if align.query_alignment_length < rlen_min or align.query_alignment_length > rlen_max:
-                        continue
                     # note: both reference_start and reference_positions are 0-based
                     a.query_sequence  = align.query_alignment_sequence[-(p + 1)]
                     a.reference_start = align.get_reference_positions()[-(p + 1)]
--- psite/predict.py
+++ psite/predict.py
@@ -5,7 +5,8 @@ import click
 import pandas as pd
 import numpy as np
 import pysam
-from psite.utils import read_fasta, chunk_iter, rev_comp, CLICK_CS
+# from psite.utils import read_fasta, chunk_iter, rev_comp, CLICK_CS
+from utils import read_fasta, chunk_iter, rev_comp, CLICK_CS
 
 
 @click.command(context_settings=CLICK_CS)
@@ -13,21 +14,17 @@ from psite.utils import read_fasta, chunk_iter, rev_comp, CLICK_CS
 @click.argument('path_bam', type=click.STRING)
 @click.argument('path_model', type=click.STRING)
 @click.argument('path_out', type=click.STRING)
-@click.option('-c', '--chunk_size', type=click.INT, default=65536,
-              help='chunk size for prediction batch')
 @click.option('-i', '--ignore_txversion', is_flag=True, default=False,
               help='whether to ignore trasncript version in ".\d+" format')
-@click.option('-l', '--rlen_min', type=click.INT, default=27,
-              help='lower bound for RPF mapped length')
-@click.option('-u', '--rlen_max', type=click.INT, default=32,
+@click.option('-l', '--rlen_min', type=click.INT, default=None,
+              help='lower bound for mapped mapped length')
+@click.option('-u', '--rlen_max', type=click.INT, default=None,
               help='upper bound for mapped read length')
-@click.option('-n', '--nts', type=click.INT, default=1,
-              help='fanking nucleotides to consider at each side')
-@click.option('-p', '--threads', type=click.INT, default=1,
-              help='Number of threads used for prediction')
+@click.option('-c', '--chunk_size', type=click.INT, default=100000,
+              help='chunk size for prediction batch')
 def predict(path_ref, path_bam, path_model, path_out,
-            ignore_txversion=False, nts=3, chunk_size=1024,
-            rlen_min=25, rlen_max=35, threads=1):
+            ignore_txversion=False, chunk_size=100000,
+            rlen_min=None, rlen_max=None):
     """
     load pre-trained model and predict P-site offsets
     
@@ -41,12 +38,17 @@ def predict(path_ref, path_bam, path_model, path_out,
     print('...load ref and model', file=sys.stderr)
     ref = read_fasta(path_ref, ignore_version=ignore_txversion)
     model = pickle.load(open(path_model, 'rb'))
-    model.n_jobs = threads
+    nts = model.nts
+
+    # set read length cutoffs based on those used for training if not set
+    if rlen_min == None:
+        rlen_min = model.qwidth_range.min()
+    if rlen_max == None:
+        rlen_max = model.qwidth_range.max()
 
     # prediction
     print('...Parse bam by chunk and predict', file=sys.stderr)
-    batch_cols = ['qwidth'] + [f'{i}{j}' for i in ['s', 'e'] for j in range(2*nts)]
-    features = ['qwidth'] + [f'{i}{j}_{k}' for i in ['s', 'e'] for j in range(2*nts) for k in 'ACGT']
+    nuc_cols = [f's{i}' for i in range(2*nts)]
 
     with pysam.AlignmentFile(path_bam) as bam:
         output = pysam.AlignmentFile(path_out, "wb", header=bam.header)
@@ -57,37 +59,32 @@ def predict(path_ref, path_bam, path_model, path_out,
             if len(aligns) == 0:
                 continue
             qlen = np.zeros(len(aligns), dtype=int)
-            seqs_start = [None] * len(aligns)
-            seqs_end = [None] * len(aligns)
+            nucs = [None] * len(aligns)
             for i, align in enumerate(aligns):
                 seq = ref[align.reference_name]
                 qlen[i] = align.query_alignment_length
-                if align.reference_start - nts >= 0:
-                    seq_left = seq[(align.reference_start - nts):(align.reference_start + nts)]
-                else:
-                    seq_left = seq[:(align.reference_start + nts)].rjust(2*nts, '-')
-                if align.reference_end + nts <= len(seq):
-                    seq_right = seq[(align.reference_end - nts):(align.reference_end + nts)]
-                else:
-                    seq_right = seq[(align.reference_end - nts):].ljust(2*nts, '-')
                 if align.is_reverse:
-                    seqs_start[i] = rev_comp(seq_right)
-                    seqs_end[i] = rev_comp(seq_left)
+                    if align.reference_end + nts <= len(seq):
+                        flank_5p = seq[(align.reference_end - nts):(align.reference_end + nts)]
+                    else:
+                        flank_5p = seq[(align.reference_end - nts):].ljust(2*nts, '-')
+                    nucs[i] = rev_comp(flank_5p)
                 else:
-                    seqs_start[i] = seq_left
-                    seqs_end[i] = seq_right
-            seqs_start = pd.DataFrame([list(i) for i in seqs_start])
-            seqs_end = pd.DataFrame([list(i) for i in seqs_end])
+                    if align.reference_start - nts >= 0:
+                        flank_5p = seq[(align.reference_start - nts):(align.reference_start + nts)]
+                    else:
+                        flank_5p = seq[:(align.reference_start + nts)].rjust(2*nts, '-')
+                    nucs[i] = flank_5p
+            nucs = pd.DataFrame([list(i) for i in nucs])
             # index needed for edge case of one-row data frame
             batch = pd.DataFrame({'qwidth': qlen}, index=range(len(qlen)))
-            batch = pd.concat([batch, seqs_start, seqs_end], axis = 1)
-            batch.set_axis(batch_cols, axis=1, inplace=True)
-            batch = pd.get_dummies(batch)
-            X = np.zeros((batch.shape[0], 1 + 16 * nts), dtype=int)
-            for k, column in enumerate(features):
-                if column in list(batch.columns.values):
-                    X[:,k] = batch.loc[:, column]
-            p_sites = model.predict(X)
+            batch = pd.concat([batch, nucs], axis = 1)
+            batch.set_axis(['qwidth'] + nuc_cols, axis=1, inplace=True)
+            for i in nuc_cols:
+                batch.loc[:, i] = pd.Categorical(batch[i], categories=['A', 'C', 'G', 'T'])
+            batch = pd.get_dummies(batch, drop_first=True)
+            
+            p_sites = model.predict(batch)
             for p, align in zip(p_sites, aligns):
                 if align.query_alignment_length >= rlen_min and align.query_alignment_length <= rlen_max:
                     align.set_tag('PS', p, 'i')
